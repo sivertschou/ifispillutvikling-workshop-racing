4ff6a15142434c090cad34f4176e0cd7d505f745
sivertschou <sivertschou@hotmail.com>
2018-09-12 10:17:42 +0200

    Løsningsforslag added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	[SerializeField] Transform carTrans = null;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = carTrans.position + new Vector3(0f,0f,-10f);
+	}
+}
